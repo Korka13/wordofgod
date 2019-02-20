a47786547501eee0a7f6a8e62086f3fb47db7dfe
Korka13 <42845465+Korka13@users.noreply.github.com>
2019-02-20 06:40:10 +0200

    Add files via upload

--- js/wordofgod.js
+++ js/wordofgod.js
@@ -1,17 +1,18 @@
+
 //Function that centers in viewport
-jQuery.fn.center = function() {
-  this.css("position", "absolute");
-  this.css("top", ($(window).height() - this.height()) / 2 + $(window).scrollTop() + "px");
-  this.css("left", ($(window).width() - this.width()) / 2 + $(window).scrollLeft() + "px");
-  return this;
-}
+// jQuery.fn.center = function() {
+//   this.css("position", "absolute");
+//   this.css("top", ($(window).height() - this.height()) / 2 + $(window).scrollTop() + "px");
+//   this.css("left", ($(window).width() - this.width()) / 2 + $(window).scrollLeft() + "px");
+//   return this;
+// }
 
 $(".nulla").on('animationend', function() { 
     $(".nulla").removeClass("big").css({"display": "none"});
   });
 
 //Center
-$(".nulla").center()
+// $("img").center()
 
 //Zoom in / out
 $("html").click(function() {{
@@ -20,15 +21,11 @@ $("html").click(function() {{
 },5000);
 }
 	var obj = document.createElement("audio");
-  obj.src = "img/nulla3.mp3";
+  obj.src = "media/nulla3.mp3";
 setTimeout(function() {
 	$(".nulla").addClass("big").css({"display": "initial"});
 	obj.play();
-  $(".sunburst").fadeIn(2000, function() {$(this).fadeOut(2000);});
+  $(".sunburst").fadeIn(1500, function(){$(this).fadeOut(1500)});
 }, 2000);
 
 });;
-
-// $("html").click(function(){
-//   $('.nulla').show("scale",{}, 5000);
-// })
